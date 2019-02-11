17f5bc9b506c24bd13c95109f91b6280df2fc725
TheToiletTyrant <316thebrains@gmail.com>
2019-02-11 15:14:10 -0500

    Changed Game

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const version = "6.0"
 const memeCount = 22;
 const token1 = process.env.token;
 const token2 = process.env.token2;
-const game = `/help`
+const game = `Fortnut`
 const status = "online"/* online, dnd, offline, idle*/
 
 //embedColors
